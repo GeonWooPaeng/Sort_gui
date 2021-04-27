5ac0fee69f49d27d9ef25aba3b93eeee280a5433
GeonWooPaeng <gwpaeng@naver.com>
2021-04-27 15:07:09 +0900

    test

---
+++ README.txt
@@ -0,0 +1,7 @@
+
+https://nadocoding.tistory.com/9
+
+1. gui 만들기
+2. 정렬 5가지 구현
+3. txt file 읽고 저장(input, output)
+4. gui 노래 & 정렬 과정 넣기
---
+++ Sort_gui.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+window = Tk()
+window.title("AL Form Project 201010298 팽건우")
+window.geometry("1080x720") #가로 * 세로
+
+
+sort_label = Label(window, 
+                text="정렬방식",
+                font = ("Arial", "30", "italic")).pack()
+
+# input_label = Label(window,
+#                 )
+# output_label = Label(window,
+#                   )
+
+
+window.mainloop()
