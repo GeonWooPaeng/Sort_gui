216c1ec979de939c5b635e8624aa87f6c5125a34
GeonWooPaeng <gwpaeng@naver.com>
2021-05-09 18:47:36 +0900

    selection, bubble, insert complete

--- README.txt
+++ README.txt
@@ -1,11 +1,24 @@
 
 https://nadocoding.tistory.com/9
 
+- graph - 
+https://btyy.tistory.com/entry/19%EC%9D%BC%EC%B0%A8-python-%EA%B7%B8%EB%9E%98%ED%94%BD-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC-%EC%BA%94%EB%B2%84%EC%8A%A4-%EA%B1%B0%ED%92%88%EC%A0%95%EB%A0%AC-%ED%91%9C%ED%98%84
+https://mindscale.kr/course/python-visualization-basic/animation/
+https://holypython.com/python-visualization-tutorial/creating-bar-chart-animations/
+https://www.delftstack.com/ko/howto/matplotlib/how-to-plot-in-real-time-using-matplotlib/
+https://infinitt.tistory.com/49
+http://www.gisdeveloper.co.kr/?p=10204
+
 1. gui 만들기 0
 2. 정렬 5가지 구현 0
-3. txt file 읽고 저장(input, output)
-4. gui 노래 & 정렬 과정 넣기
+3. txt file 읽고 저장(input, output) 0
+4. gui 노래 & 정렬 과정 넣기(그래픽)
+- 그래픽을 거북이로 움직이는 것으로 해야겠습니다.
 
+<순서>
+1. 
+find input button 입력을 통해 input.txt를 찾아주세요
+(input.txt file 입력은 세로로 입력 + 빈칸 없이 입력해주세요)
 
-input.txt file 입력을 
-세로로 입력 + 빈칸 없이 입력해주세요
+2. 
+어떤 정렬을 할지 선택해 주세요
--- Sort_gui.py
+++ Sort_gui.py
@@ -2,17 +2,132 @@ import copy
 import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import matplotlib.animation as ani
 from tkinter import filedialog
 
 
-window = tk.Tk()
-window.title("AL Form Project 201010298 팽건우")
-window.geometry("1080x720") #가로 * 세로
+
+class Bubble_sort(tk.Frame):
+
+	# def bubble_sort(self, arr):
+	# 	for i in range(1, len(arr)):
+	# 		for j in range(len(arr) - i):
+	# 			if arr[j] > arr[j + 1]:
+	# 				arr[j], arr[j+1] = arr[j+1], arr[j]
+	# 	return arr
+
+	def __init__(self, lists, master=None):
+		super().__init__(master)
+		self.lists = lists
+		self.grid(row=40,column=12)
+		self.create_widgets()
+		
+	def create_widgets(self):
+		self.draw = tk.Canvas(self)
+		self.draw['height'] = 500
+		self.draw['width'] = 500
+		self.draw.grid(row=40,column=12)
+		for x in range(len(self.lists)):
+			self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='black')
+		self.animate()
+
+	def animate(self):
+		self.draw.delete(tk.ALL)
+		for i in range(len(self.lists)-1):
+			if self.lists[i] > self.lists[i+1]:
+				self.lists[i], self.lists[i+1] = self.lists[i+1], self.lists[i]
+			
+		for x in range(len(self.lists)):
+			self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='black')
+
+		self.draw.after(100, self.animate)
+
+class	Selection_sort(tk.Frame):
+	# def selection_sort(self, arr):
+	# 	x = 0
+	# 	for i in range(len(arr) - 1):
+	# 		least = i
+	# 		for j in range(i+1, len(arr)):
+	# 			if arr[j] < arr[least]:
+	# 				least = j
+	# 		arr[i], arr[least] = arr[least], arr[i]
+	# 	return arr
+
+	def __init__(self, lists, master=None):
+		super().__init__(master)
+		self.lists = lists
+		self.grid(row=40,column=12)
+		self.create_widgets()
+
+	def create_widgets(self):
+		self.draw = tk.Canvas(self)
+		self.draw['height'] = 500
+		self.draw['width'] = 500
+		self.draw.grid(row=40, column=12)
+		for x in range(len(self.lists)):
+			self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='red')
+		self.animate()
+
+	def least_idx(self, start):
+		least_idx = start
+
+		for i in range(start + 1, len(self.lists)):
+			if self.lists[i] < self.lists[least_idx]:
+				least_idx = i
+
+		return least_idx
+
+	def animate(self, i=0):
+		if i < len(self.lists):
+			self.draw.delete(tk.ALL)
+			least = self.least_idx(i)
+			self.lists[i], self.lists[least] = self.lists[least], self.lists[i]	
+
+			for x in range(len(self.lists)):
+				self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='red')
+
+			self.draw.after(100, self.animate, i+1)
+
+class Insert_sort(tk.Frame):
+
+	
+	# def insert_sort(self, arr):
+	# 	for i in range(1, len(arr)):
+	# 		for j in range(i, 0, -1):
+	# 			if arr[j] < arr[j - 1]:
+	# 				arr[j], arr[j - 1] = arr[j - 1], arr[j]
+	# 	return arr
+
+	def __init__(self, lists, master=None):
+		super().__init__(master)
+		self.lists = lists
+		self.grid(row=40,column=12)
+		self.create_widgets()
+
+
+	def create_widgets(self):
+		self.draw = tk.Canvas(self)
+		self.draw['height'] = 500
+		self.draw['width'] = 500
+		self.draw.grid(row=40, column=12)
+		for x in range(len(self.lists)):
+			self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='red')
+		self.animate()
+
+	def animate(self,i=0):
+		if i < len(self.lists)-1:
+			self.draw.delete(tk.ALL)
+			for j in range(i, 0, -1):
+				if self.lists[j] > self.lists[j+1]:
+					self.lists[j], self.lists[j+1] = self.lists[j+1], self.lists[j]
+			
+			for x in range(len(self.lists)):
+				self.draw.create_oval(x, self.lists[x], x+5, self.lists[x]+5, fill='black')
+
+			self.draw.after(100, self.animate,i+1)
 
 class Sort_Gui:
 	def __init__(self):
-
 		self.sort_var = tk.IntVar()
 
 		sort_label = tk.Label(window, 
@@ -34,31 +149,30 @@ class Sort_Gui:
 	
 		input_button = tk.Button(window, text="find input file", width=20, command=self.find_file).grid(row=20, column=12)
 		output_button = tk.Button(window, text="save result", width=20, command=self.save_file).grid(row=20, column=18)
-		# sort_start_button = tk.Button(window, text="Sort Start", width=20, command=self.sort_start).grid(row=30, column=12)
+		# play_button = tk.Button(window, text="play sort", width=20, command=self.play_sort).grid(row=30, column=12)
 
 		window.mainloop()
-
-	# def sort_start(self):
-	# 	if (self.sort_num == 1):
-
 
 	def sort_select(self):
 		self.sort_num = self.sort_var.get()
 		if (self.sort_num == 1):
-			print(self.selection_sort(self.numbers))
+			# print(self.selection_sort(self.numbers))
 			self.result = "선택 정렬\n" + ' '.join(map(str,self.numbers))
 			self.numbers = copy.deepcopy(self.nums)
 			self.sort_show.config(text="선택 정렬")
+			Selection_sort(self.numbers, master=None)
 		elif (self.sort_num == 2):
-			print(self.bubble_sort(self.numbers))
+			# print(self.bubble_sort(self.numbers))
 			self.result = "버블 정렬\n" + ' '.join(map(str,self.numbers))
 			self.numbers = copy.deepcopy(self.nums)
 			self.sort_show.config(text="버블 정렬")
+			Bubble_sort(self.numbers, master=None)
 		elif (self.sort_num == 3):
-			print(self.insert_sort(self.numbers))
+			# print(self.insert_sort(self.numbers))
 			self.result = "삽입 정렬\n" + ' '.join(map(str,self.numbers))
 			self.numbers = copy.deepcopy(self.nums)
 			self.sort_show.config(text="삽입 정렬")
+			Insert_sort(self.numbers, master=None)
 		elif (self.sort_num == 4):
 			print(self.merge_sort(self.numbers))
 			self.result = "병합 정렬\n" + ' '.join(map(str,self.numbers))
@@ -70,28 +184,9 @@ class Sort_Gui:
 			self.numbers = copy.deepcopy(self.nums)
 			self.sort_show.config(text="퀵 정렬")
 
-	def selection_sort(self, arr):
-		for i in range(len(arr) - 1):
-			least = i
-			for j in range(i+1, len(arr)):
-				if arr[j] < arr[least]:
-					least = j
-			arr[i], arr[least] = arr[least], arr[i]
-		return arr
 
-	def bubble_sort(self, arr):
-		for i in range(1, len(arr)):
-			for j in range(len(arr) - i):
-				if arr[j] > arr[j + 1]:
-					arr[j], arr[j+1] = arr[j+1], arr[j]
-		return arr
 
-	def insert_sort(self, arr):
-		for i in range(1, len(arr)):
-			for j in range(i, 0, -1):
-				if arr[j] < arr[j - 1]:
-					arr[j], arr[j - 1] = arr[j - 1], arr[j]
-		return arr
+
 
 	def merge_sort(self, arr):
 		if len(arr) < 2:
@@ -150,8 +245,22 @@ class Sort_Gui:
 		output_file = open('output.txt', "w", encoding='UTF-8')
 		output_file.write(self.result)
 		output_file.close()
-		
-
-Sort_Gui()
 
 
+
+
+if __name__ == '__main__':
+	window = tk.Tk()
+	window.title("AL Form Project 201010298 팽건우")
+	window.geometry("1080x720") #가로 * 세로
+	sort_gui = Sort_Gui()
+	# sort_gui.mainloop()
+
+
+
+# unorder = [ 18, 195, 177, 74, 84, 183, 147, 95, 3, 157, 173, 176, 181, 168, 139, 21, 143, 101, 94, 43, 93, 91, 169, 171, 78, 41, 50, 26, 178, 194, 33, 170, 30, 118, 112, 77, 69, 1, 150, 35]
+
+# root = tk.Tk()
+# root.title("그리기 객체")
+# app=Bubble_sort(unorder, master=root)
+# app.mainloop()
